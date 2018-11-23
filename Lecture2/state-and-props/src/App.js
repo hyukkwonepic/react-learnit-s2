fb21c7c352b06dc74816aaa238bbc04a562605bd
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2018-11-23 17:21:27 +0900

    write up state-and-props app

--- Lecture2/state-and-props/src/App.js
+++ Lecture2/state-and-props/src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
+import List from './components/List';
+import Color from './components/Color';
+import Search from './components/Search';
+import Tags from './components/Tags';
+
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World!
+        {/* <List /> */}
+        {/* <Color /> */}
+        {/* <Search /> */}
+        <Tags />
       </div>
     );
   }
