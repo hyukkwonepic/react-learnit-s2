3275f934deca34f2c39d84a18ee849c9e9ceb9e2
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2018-12-08 10:54:08 +0900

    added async code

--- Lecture4/async/callback2.js
+++ Lecture4/async/callback2.js
@@ -1,8 +1,8 @@
 // 1-1. callback과 onSuccess, onFailure
 function getTime(onSuccess, onFailure) {
   setTimeout(() => {
-    const isSuccess = true;
-    // const isSuccess = Math.random() >= 0.5;
+    // const isSuccess = true;
+    const isSuccess = Math.random() >= 0.5;
 
     if (isSuccess) {
       onSuccess(new Date());
@@ -13,20 +13,10 @@ function getTime(onSuccess, onFailure) {
 }
 
 function execute() {
-  // callback hell...
-  getTime((currentTime) => {
-    console.log('The current time is ', currentTime);
-    getTime((newTime) => {
-      console.log('The new time is', newTime);
-      getTime((realNewTime) => {
-        console.log('The real new time is', realNewTime);
-        getTime((pheww) => {
-          console.log('phewww...', pheww);
-        })
-      })
-    })
-  }, (error) => {
-    console.log(error);
+  getTime((time) => {
+    console.log('The current time!' + time);
+  }, (e) => {
+    console.log('An error', e);
   });
 }
 
